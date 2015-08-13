97b1886a1f867c3fe6f75fecab9f510f7300c2a9
mathieujofis <mathieujofis@gmail.com>
2015-08-13 15:38:19 -0700

    update EmailBoxFunctions, add Message object

--- EmailBoxFunctions.cs
+++ EmailBoxFunctions.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
 using System.Collections.Generic;
@@ -7,37 +7,84 @@ public class EmailBoxFunctions : MonoBehaviour {
 	[SerializeField] ContentSizeFitter contentSizeFitter;
 	[SerializeField] Transform messageParentPanel;
 	[SerializeField] GameObject newMessagePrefab;
-	
-	string message = "";
-	
-	List<GameObject> messageList;
 
-	// Use this for initialization
-	void Start () 
-	{
-		messageList = new List<GameObject> ();
-	}
+    float timeLimit;
+	string messageText = "";
+
+    List<Message> messages;
+	List<GameObject> messageGameObjs;
+
+    // Use this for initialization
+    void Start () 
+    {
+
+        initMessages();
+        timeLimit = 3;
+        messageGameObjs = new List<GameObject> ();
+    }
+
+
+    void Update () 
+    {
+
+        timeLimit -= Time.deltaTime;
+        
+        if (timeLimit <= 0)
+        {
+            ShowEmailMessage();
+            timeLimit = 4;
+        }
+
+    }
 	
-	public void setMessage(string message)
+	public void ShowEmailMessage ()
 	{
-		this.message = message;
-	}
-	
-	public void ShowMessage ()
-	{
+        string message = messages [Random.Range(0, 10)].getMessage();
+        //Debug.Log(message);
 		if (message != "") 
 		{
-			if (messageList.Count > 4) 
+			if (messageGameObjs.Count > 11) 
 			{
-				Destroy(messageList[0]);
-				messageList.RemoveAt(0);
+				Destroy(messageGameObjs[0]);
+				messageGameObjs.RemoveAt(0);
 			}
 			GameObject clone = Instantiate (newMessagePrefab);
-			messageList.Add(clone);
+			messageGameObjs.Add(clone);
 			clone.transform.SetParent (messageParentPanel);
 			clone.transform.SetSiblingIndex (messageParentPanel.childCount - 2);
 			clone.GetComponent<MessageFunctions> ().ShowMessage (message);
 		}
 		
 	}
+
+    public void initMessages()
+    {
+        messages = new List<Message>();
+        messages.Add(new Message("Subject: New Video",false));
+        messages.Add(new Message("Subject: Your Amazon.com order",false));
+        messages.Add(new Message("Subject: LinkedIn Updates",false));
+        messages.Add(new Message("Subject: See your latest Chase Freedom cash back offers",false));
+        messages.Add(new Message("Subject: Fw: Internship opportunities with Microsoft",false));
+        messages.Add(new Message("Subject: Fw: Research Scientist Positions at NVIDIA",false));
+        messages.Add(new Message("Subject: Re: meeting tomorrow?",false));
+        messages.Add(new Message("Subject: Re: can we meet next week?",false));
+        messages.Add(new Message("Subject: 4 Days only! Save Big on Dolce & Gabbana",false));
+        messages.Add(new Message("Subject: Comedy at Chumash, SBYPC Summer Soiree, ...",false));
+        messages.Add(new Message("",false));
+        messages.Add(new Message("",false));
+        messages.Add(new Message("",false));
+        messages.Add(new Message("",false));
+        messages.Add(new Message("",false));
+        messages.Add(new Message("",false));
+        messages.Add(new Message("",false));
+        messages.Add(new Message("",false));
+        messages.Add(new Message("",false));
+        messages.Add(new Message("",false));
+        messages.Add(new Message("",false));
+        messages.Add(new Message("",false));
+        messages.Add(new Message("",false));
+        messages.Add(new Message("",false));
+        messages.Add(new Message("",false));
+
+    }
 }
